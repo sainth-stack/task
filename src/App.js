dab913abaf1fae97591f57826c43fe56afad0833
unknown <123sainathreddy100@gmail.com>
2022-01-12 17:50:13 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import './App.css'
 import Theme2 from './pages/theme2'
 import Buttons from './components/buttons'
-import Album from "./components/theme"
 import Home from './pages/theme1'
 import {
   BrowserRouter as Router,
